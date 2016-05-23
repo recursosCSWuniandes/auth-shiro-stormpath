73c3da528957eed4c1af0c482dab355ea74b451d
Andrés Esguerra <afesguerra@outlook.com>
2016-05-23 17:34:54 -0500

    Refactor CORSFilter Se refactoriza cors filter para mantenibilidad

--- src/main/java/co/edu/uniandes/csw/auth/filter/CORSFilter.java
+++ src/main/java/co/edu/uniandes/csw/auth/filter/CORSFilter.java
@@ -7,15 +7,18 @@ import org.apache.shiro.web.util.WebUtils;
 
 /**
  * Enables Cross Site Resource Sharing
+ *
  * @author af.esguerra10
  */
 public class CORSFilter extends PathMatchingFilter {
 
+    private static final String ORIGIN_VAR = "ALLOW_ORIGIN";
+
     @Override
     protected boolean onPreHandle(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
-        String allowOrigin="http://localhost:9000";
-        if (System.getenv("ALLOW_ORIGIN")!=null){
-            allowOrigin=System.getenv("ALLOW_ORIGIN");
+        String allowOrigin = System.getenv(ORIGIN_VAR);
+        if (allowOrigin == null) {
+            allowOrigin = "http://localhost:9000";
         }
         WebUtils.toHttp(response).setHeader("Access-Control-Allow-Origin", allowOrigin);
         WebUtils.toHttp(response).setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
